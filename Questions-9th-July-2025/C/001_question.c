c67f5153e32ccb2e334414e6a47d61c1f0eb1b54
Arya Gupta <aryagupta164@gmail.com>
2025-07-11 06:22:13 +0530

    Fixed question 1 solution with constraints

--- Questions-9th-July-2025/C/001_question.c
+++ Questions-9th-July-2025/C/001_question.c
@@ -3,7 +3,10 @@
 int main() {
     int num;
     printf("Enter the number : ");
-    scanf("%d", &num);
+    do{
+        scanf("%d", &num);
+        if(num<0) printf("Negative value entered!! Try Again\n");
+    }while(num<0);    
     printf((num%2) ? "Odd number" : "Even number");
     return 0;
 }
--- Questions-9th-July-2025/Python/001_question.py
+++ Questions-9th-July-2025/Python/001_question.py
@@ -1,6 +1,7 @@
 def main():
     # TODO: Implement solution here
     n = int(input("Enter a number : "))
+    while(n<0) : n = int(input("INVALID VALUE ENTERED!! \nEnter a positive value : "))
     if n%2==0 : print(n, "is even")
     else : print(n, "is odd")
 
