b0c932e0f6ba416f435a5408a415eef2d7777437
Arya Gupta <aryagupta164@gmail.com>
2025-07-12 22:23:27 +0530

    Solved first 10 programs in C

--- Questions-9th-July-2025/C/005_question.c
+++ Questions-9th-July-2025/C/005_question.c
@@ -2,5 +2,18 @@
 
 int main() {
     // TODO: Implement solution here
+    int n = 0, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    if(n<0){
+        printf("OUT OF GIVEN CONSTRAINT.");
+        return 1;
+    }
+    while(n){
+        sum += n%10;
+        n /= 10;
+    }
+    printf("Sum of digits: %d", sum);
     return 0;
 }
